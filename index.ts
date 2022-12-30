d76b75c70a8cede29d63c1518cf5f5818b4effef
NikitaKlim <nikitaklimukwot@gmail.com>
2022-12-30 13:41:38 +0100

    0.1 TS types

---
+++ index.ts
@@ -0,0 +1,14 @@
+//let userName: string = "Ivan";
+
+// userName.isInteger();
+// userName = 5;
+
+const isBirthday: boolean = true;
+let age: number;
+const username: string = "John";
+
+age = 40;
+
+if (isBirthday) {
+    console.log(`Congrats ${username.toUpperCase()}, age: ${age + 1}`);
+}
