4caa94925b402e46e121972dfd2ee308ea4b07b0
NikitaKlim <nikitaklimukwot@gmail.com>
2023-01-03 10:53:56 +0100

    0.4 never types

--- index.ts
+++ index.ts
@@ -1,43 +1,18 @@
-//let userName: string = "Ivan";
-
-// userName.isInteger();
-// userName = 5;
-
 const isBirthdayData: boolean = true;
 let ageData: number = 40;
 const userNameData: string = "John";
 
-// Лучше избегать таких записей и сразу указывать тип
-let salary;
-salary = 5000;
-
-// function logBrtMsg(isBirthday: boolean, username: string, age: number): string {
-//     if (isBirthday) {
-//         return `Congrats ${username.toUpperCase()}, age: ${age + 1}`;
-//     } else {
-//         return "error";
-//     }
-// }
-
-const userData =
-    '{"isBirthdayData": true, "ageData": 40, "userNameData": "John"}';
-
-const userObj: {
-    isBirthdayData: boolean;
-    userNameData: string;
-    ageData: number;
-} = JSON.parse(userData);
-
-const logBrtMsg = (
-    isBirthday: boolean,
-    username: string,
-    age: number
-): string => {
-    if (isBirthday) {
-        return `Congrats ${username.toUpperCase()}, age: ${age + 1}`;
-    } else {
-        return "error";
-    }
+const createError = (msg: string) => {
+    throw new Error(msg);
 };
+
+function logBrtMsg(isBirthday: boolean, username: string, age: number): string {
+    if (isBirthday === true) {
+        return `Congrats ${username.toUpperCase()}, age: ${age + 1}`;
+    } else if (isBirthday === false) {
+        return "Too bad";
+    }
+    return createError("Error");
+}
 
 logBrtMsg(isBirthdayData, userNameData, ageData);
