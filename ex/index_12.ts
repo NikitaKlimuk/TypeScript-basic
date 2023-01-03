5dcac63f657f9d8f2c5930b82409fb04b8c4cd6d
NikitaKlim <nikitaklimukwot@gmail.com>
2023-01-03 11:08:17 +0100

    0.5 null & undefined

---
+++ ex/index_12.ts
@@ -0,0 +1,17 @@
+const test: null = null;
+const test2: any = null;
+
+// Ошибка с включенной проверкой
+const test3: string = null;
+const test4: number = null;
+
+function getRandomData() {
+    if (Math.random() < 0.5) {
+        return null;
+    } else {
+        return "  Some data     ";
+    }
+}
+
+const data = getRandomData();
+const trimmedData = data ? data.trim() : null;
