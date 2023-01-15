4ee7fae0cd442217e7d6381813b4ce6db2c3383c
Nikita Klimuk <nikitaklimukwot@gmail.com>
2023-01-15 13:33:02 +0100

    0.15 string[] type of []

--- ex/index_12.ts
+++ ex/index_12.ts
@@ -5,6 +5,10 @@ const test2: any = null;
 const test3: string = null;
 const test4: number = null;
 
+const test5: undefined = undefined;
+const test6: any = undefined;
+const test7: number = undefined;
+
 function getRandomData() {
     if (Math.random() < 0.5) {
         return null;
