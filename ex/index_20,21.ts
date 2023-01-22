a4b87ee5e015f256ce894df03ba2efa63a92c344
Nikita Klimuk <nikitaklimukwot@gmail.com>
2023-01-22 14:36:06 +0100

    object literations and annotations

--- ex/index_20,21.ts
+++ ex/index_20,21.ts
@@ -2,27 +2,39 @@ let msg: "Hello" = "Hello";
 
 msg = "Hello";
 
+const serverConfig: {
+    protocol: "https" | "http";
+    port: 3000 | 3001;
+    // start: () => {};
+} = {
+    protocol: "https",
+    port: 3001,
+};
+
 const port3000: number = 3000;
 const port3001: number = 3001;
 
-function startServer(
+const startServer: (protocol: "http" | "https", port: 3000 | 3001) => string = (
     protocol: "http" | "https",
     port: 3000 | 3001
-): "Server started" {
+): "Server started" => {
     if (port === port3000 || port === port3001) {
         console.log(`Server started on ${protocol}://server:${port}`);
     } else {
         console.error("Some errors here");
     }
     return "Server started";
-}
+};
 
-startServer("http", 3000);
+startServer(serverConfig.protocol, serverConfig.port);
+
+type AnimationTimingFunc = "ease" | "ease-out" | "ease-in";
+type AnimationID = string | number;
 
 function createAnimation(
-    id: string | number,
+    id: AnimationID,
     animName: string,
-    timingFunc: "ease" | "ease-out" | "ease-in" = "ease",
+    timingFunc: AnimationTimingFunc = "ease",
     duration: number,
     iterCount: "infinite" | number
 ): void {
