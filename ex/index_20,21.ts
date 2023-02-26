6663ff935fef7ac054146c29ebc32fa85d539c47
Nikita Klimuk <nikitaklimukwot@gmail.com>
2023-02-26 13:30:57 +0100

    24. interfaces

--- ex/index_20,21.ts
+++ ex/index_20,21.ts
@@ -1,45 +1,50 @@
-let msg: "Hello" = "Hello";
-
-msg = "Hello";
-
-type Config = {
+interface IConfig {
   protocol: "https" | "http";
   port: 3000 | 3001;
-};
-type Role = {
-  role: string;
-};
-type ConfigWithRole = Config & Role;
+  log: (msg: string) => void;
+}
 
-const serverConfig: ConfigWithRole = {
+interface IRole {
+  role: string;
+}
+
+interface IConfigWithRole extends IConfig, IRole {}
+
+const serverConfig: IConfigWithRole = {
   protocol: "https",
   port: 3001,
   role: "admin",
+  log: (msg: string): void => console.log(msg),
 };
 
-const backupConfig: Config = {
-  protocol: "http",
-  port: 3000,
-};
+// const backupConfig: IConfig = {
+//   protocol: "http",
+//   port: 3000,
+// };
 
 const port3000: number = 3000;
 const port3001: number = 3001;
 
-type StartFunction = (protocol: "http" | "https", port: 3000 | 3001) => string;
+type StartFunction = (
+  protocol: "http" | "https",
+  port: 3000 | 3001,
+  log: (msg: string) => void
+) => string;
 
 const startServer: StartFunction = (
   protocol: "http" | "https",
-  port: 3000 | 3001
+  port: 3000 | 3001,
+  log: (msg: string) => void
 ): "Server started" => {
   if (port === port3000 || port === port3001) {
-    console.log(`Server started on ${protocol}://server:${port}`);
+    log(`Server started on ${protocol}://server:${port}`);
   } else {
     console.error("Some errors here");
   }
   return "Server started";
 };
 
-startServer(serverConfig.protocol, serverConfig.port);
+startServer(serverConfig.protocol, serverConfig.port, serverConfig.log);
 
 type AnimationTimingFunc = "ease" | "ease-out" | "ease-in";
 type AnimationID = string | number;
@@ -60,3 +65,13 @@ function createAnimation(
 }
 
 createAnimation("id", "fade", "ease-in", 5, "infinite");
+
+interface Styles {
+  [key: string]: string;
+}
+
+const styles = {
+  position: "absolute",
+  top: "20px",
+  left: "35px",
+};
