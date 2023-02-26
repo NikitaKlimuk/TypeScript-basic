8c1e33196ede5b25a1ec900af4f914cb3e0f7bf6
Nikita Klimuk <nikitaklimukwot@gmail.com>
2023-02-26 12:57:38 +0100

    intersection types

--- ex/index_20,21.ts
+++ ex/index_20,21.ts
@@ -2,28 +2,41 @@ let msg: "Hello" = "Hello";
 
 msg = "Hello";
 
-const serverConfig: {
-    protocol: "https" | "http";
-    port: 3000 | 3001;
-    // start: () => {};
-} = {
-    protocol: "https",
-    port: 3001,
+type Config = {
+  protocol: "https" | "http";
+  port: 3000 | 3001;
+};
+type Role = {
+  role: string;
+};
+type ConfigWithRole = Config & Role;
+
+const serverConfig: ConfigWithRole = {
+  protocol: "https",
+  port: 3001,
+  role: "admin",
+};
+
+const backupConfig: Config = {
+  protocol: "http",
+  port: 3000,
 };
 
 const port3000: number = 3000;
 const port3001: number = 3001;
 
-const startServer: (protocol: "http" | "https", port: 3000 | 3001) => string = (
-    protocol: "http" | "https",
-    port: 3000 | 3001
+type StartFunction = (protocol: "http" | "https", port: 3000 | 3001) => string;
+
+const startServer: StartFunction = (
+  protocol: "http" | "https",
+  port: 3000 | 3001
 ): "Server started" => {
-    if (port === port3000 || port === port3001) {
-        console.log(`Server started on ${protocol}://server:${port}`);
-    } else {
-        console.error("Some errors here");
-    }
-    return "Server started";
+  if (port === port3000 || port === port3001) {
+    console.log(`Server started on ${protocol}://server:${port}`);
+  } else {
+    console.error("Some errors here");
+  }
+  return "Server started";
 };
 
 startServer(serverConfig.protocol, serverConfig.port);
@@ -32,18 +45,18 @@ type AnimationTimingFunc = "ease" | "ease-out" | "ease-in";
 type AnimationID = string | number;
 
 function createAnimation(
-    id: AnimationID,
-    animName: string,
-    timingFunc: AnimationTimingFunc = "ease",
-    duration: number,
-    iterCount: "infinite" | number
+  id: AnimationID,
+  animName: string,
+  timingFunc: AnimationTimingFunc = "ease",
+  duration: number,
+  iterCount: "infinite" | number
 ): void {
-    const elem = document.querySelector(`#${id}`) as HTMLElement;
+  const elem = document.querySelector(`#${id}`) as HTMLElement;
 
-    if (elem) {
-        console.log(`${animName} ${timingFunc} ${duration} ${iterCount}`);
-        elem.style.animation = `${animName} ${timingFunc} ${duration} ${iterCount}`;
-    }
+  if (elem) {
+    console.log(`${animName} ${timingFunc} ${duration} ${iterCount}`);
+    elem.style.animation = `${animName} ${timingFunc} ${duration} ${iterCount}`;
+  }
 }
 
 createAnimation("id", "fade", "ease-in", 5, "infinite");
