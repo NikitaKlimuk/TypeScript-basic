d76ac045318c2674f0fa6aef376607f28139d8d2
Nikita Klimuk <nikitaklimukwot@gmail.com>
2023-04-03 14:41:25 +0200

    add Function Overloading section

--- ex/37_typeGuards.ts
+++ ex/37_typeGuards.ts
@@ -19,6 +19,7 @@ function isNumber(n: unknown): n is number {
 }
 
 interface Car {
+  name: "car";
   engine: string;
   wheels: {
     number: number;
@@ -27,20 +28,56 @@ interface Car {
 }
 
 interface Ship {
+  name: "ship";
   engine: string;
   sail: string;
 }
 
-function repairVehicle(vehicle: Car | Ship): void {
-  console.log(vehicle.engine);
-  if (isCar(vehicle)) {
-    vehicle.wheels;
-  } else {
-    vehicle.sail;
+interface Airplane {
+  name: "airplane";
+  engine: string;
+  wings: string;
+}
+
+interface SuperAirplane {
+  name: "smth";
+  engine: string;
+  wings: string;
+}
+
+type Vehicle = Car | Ship | Airplane | SuperAirplane;
+
+function repairVehicle(vehicle: Vehicle): void {
+  // console.log(vehicle.engine);
+  // if (isCar(vehicle)) {
+  //   vehicle.wheels;
+  // } else if (isShip(vehicle)) {
+  //   vehicle.sail;
+  // } else {
+  //   // const smth: never = vehicle;
+  //   vehicle.wings;
+  // }
+
+  switch (vehicle.name) {
+    case "car":
+      console.log(vehicle.wheels);
+      break;
+    case "ship":
+      console.log(vehicle.sail);
+      break;
+    case "airplane":
+      console.log(vehicle.wings);
+      break;
+    default:
+      console.log("Some error");
   }
 }
 
-function isCar(car: Car | Ship): car is Car {
+function isCar(car: Vehicle): car is Car {
   return "wheels" in car;
   return (car as Car).wheels.number !== undefined;
+}
+
+function isShip(ship: Vehicle): ship is Ship {
+  return "sail" in ship;
 }
